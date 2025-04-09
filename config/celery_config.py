6769d7413bfc82b3bd55ceb8212db9c83bfa6c1d
techie-guy92 <soheil.dalirii@gmail.com>
2025-04-09 12:01:40 +0330

    make some changes in celery_config

--- config/celery_config.py
+++ config/celery_config.py
@@ -4,11 +4,9 @@ from celery.schedules import crontab
 from django.conf import settings
 from logging import getLogger
 
-logger = getLogger("celery_config")
-
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-app = Celery("yummy-yard")
+app = Celery("YummyYard")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
@@ -17,10 +15,3 @@ app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 app.conf.update(
     broker_connection_retry_on_startup=True,
 )
-
-# app.conf.beat_schedule = {
-#     'check-premium-subscriptions-every-minute': {
-#         'task': 'users.tasks.check_premium_subscriptions',
-#         'schedule': crontab(hour=0, minute=0),
-#     },
-# }
