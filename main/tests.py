b447cbd14528e2a97d46d8ec54f673b7e208b6ae
techie-guy92 <soheil.dalirii@gmail.com>
2025-05-21 11:54:35 +0330

    predefined things for test

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,88 @@
-from django.test import TestCase
+import os
+os.environ["DJANGO_SETTINGS_MODULES"]= "config.settings"
+import django
+django.setup()
 
-# Create your tests here.
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
+from jwt import decode
+from django.urls import resolve, reverse
+from .models import *
+from .serializers import *
+from .views import *
+from .urls import *
+from constants import primary_user_1, primary_user_2, primary_user_3, primary_user_4, primary_user_5, invalid_user_1
+from utilities import create_test_users
+
+
+#====================================== Gategory Test ===================================================
+
+class GategoryTest(APITestCase):
+    pass
+
+
+#====================================== Product Test ====================================================
+
+class ProductTest(APITestCase):
+    pass
+
+
+#====================================== Wishlist Test ===================================================
+
+class WishlistTest(APITestCase):
+    pass
+
+
+#====================================== ShoppingCart Test ===============================================
+
+class ShoppingCartTest(APITestCase):
+    pass
+
+
+#====================================== Delivery Schedule Test ==========================================
+
+class DeliveryScheduleTest(APITestCase):
+    pass
+
+
+#====================================== Delivery Schedule Change Test ===================================
+
+class DeliveryScheduleChangeTest(APITestCase):
+    pass
+
+
+#====================================== Order Test ======================================================
+
+
+class OrderTest(APITestCase):
+    pass
+
+
+#====================================== Order Cancellation Test =========================================
+
+
+class OrderCancellationTest(APITestCase):
+    pass
+
+
+#====================================== Delivery Test ===================================================
+
+
+class DeliveryTest(APITestCase):
+    pass
+
+
+#====================================== UserView Test ===================================================
+
+class UserViewTest(APITestCase):
+    pass
+
+
+#====================================== Rating Test =====================================================
+
+class RatingTest(APITestCase):
+    pass
+
+
+#========================================================================================================
