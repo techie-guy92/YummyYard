cee510ee95c1860b6b19d1802dd468bb95c2c4d6
techie-guy92 <soheil.dalirii@gmail.com>
2025-09-06 09:24:44 +0330

    defined redis in celery task

--- config/settings.py
+++ config/settings.py
@@ -349,17 +349,17 @@ CELERY_BEAT_SCHEDULE = {
 }
 
 
-# CACHES = {
-#     "default": {
-#         "BACKEND": "django_redis.cache.RedisCache",
-#         "LOCATION": "redis://redis:6379/1",
-#         "OPTIONS": {
-#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
-#         }
-#     }
-# }
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
 
-# CACHE_TTL = 60 * 15  # 15 minutes
+CACHE_TTL = 60 * 15  
 
 
 # SOCIALACCOUNT_PROVIDERS = {
--- users/tasks.py
+++ users/tasks.py
@@ -1,4 +1,5 @@
 from celery import shared_task
+from django.core.cache import cache
 from django.utils.timezone import now, localtime
 import logging
 from .models import *
@@ -48,3 +49,34 @@ def check_premium_subscriptions():
       
 
 #========================================================================================================
+
+# @shared_task
+# def check_premium_subscriptions():
+#     try:
+#         current_date = localtime(now())
+#         logger.debug(f"Checking for expired subscriptions at {current_date}")
+
+#         expired_subscriptions = PremiumSubscription.objects.filter(expiry_date__lt=current_date)
+#         logger.info(f"Expired subscriptions: {expired_subscriptions}")
+
+#         for sub in expired_subscriptions:
+#             logger.debug(f"Processing subscription: {sub}")
+#             user = sub.user
+#             user.is_premium = False
+#             user.save()
+
+#             # Update Redis cache
+#             cache_key = f"user_{user.id}_is_premium"
+#             cache.set(cache_key, False, timeout=3600) 
+#             logger.info(f"Cached {cache_key} = False")
+
+#             sub.delete()
+#             logger.info(f"Deleted subscription: {sub}")
+
+#         logger.info(f"Checked premium subscriptions at {current_date}")
+
+#     except Exception as error:
+#         logger.error(f"Error in check_premium_subscriptions: {error}")
+
+
+# is_premium = cache.get("user_42_is_premium")
