f79dd6886fabad4f49762e752b99d4a38ae1eed0
techie-guy92 <soheil.dalirii@gmail.com>
2025-09-11 10:56:45 +0330

    removed storage_utils.py

--- config/storage_utils.py
+++
@@ -1,5 +0,0 @@
-from django.core.files.storage import get_storage_class
-from django.conf import settings
-
-def get_default_storage():
-    return get_storage_class(settings.DEFAULT_FILE_STORAGE)()
