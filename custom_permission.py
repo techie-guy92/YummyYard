4e880a9ca6775735da8de5f951e9a015dc1680a5
techie-guy92 <soheil.dalirii@gmail.com>
2025-10-07 09:57:28 +0330

    fetch user via getattr method to fetch safely

--- custom_permission.py
+++ custom_permission.py
@@ -15,37 +15,14 @@ class CheckOwnershipPermission(BasePermission):
     Methods:
     has_permission(self, request, view): Checks if the user is authenticated
     has_object_permission(self, request, view, obj): Checks if the user owns the object or has the right permissions
-    """
-    
+    """   
     def has_permission(self, request: Request, view):
-        """
-        Check if the user is authenticated.
-
-        Parameters:
-        request (Request): The request object
-        view: The view object
-
-        Returns:
-        bool: True if the user is authenticated, False otherwise
-        """
         return request.user.is_authenticated
     
     def has_object_permission(self, request: Request, view, obj):
-        """
-        Check if the user has permission to access the object.
-
-        Parameters:
-        request (Request): The request object
-        view: The view object
-        obj: The object to check permissions for
-
-        Returns:
-        bool: True if the user has permission, False otherwise
-        """
         if request.method in SAFE_METHODS:
             return True
-        return obj == request.user or obj.user == request.user
-        # return obj == request.user or getattr(obj, "user", None) == request.user
+        return obj == request.user or getattr(obj, "user", None) == request.user
 
 
 #====================================================================================================
